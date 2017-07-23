540c9435f8274230dc5ce852396df3ea54edc46a
Harish Ramachandran <harishsr@icloud.com>
2017-07-23 09:36:41 -0400

    Setup a failing test

--- test/unit/specs/list.spec.js
+++ test/unit/specs/list.spec.js
@@ -2,9 +2,10 @@ import List from '@/components/List';
 import Vue from 'vue';
 
 describe('List.vue', () => {
-
   it('displays items from the list', () => {
-    // test goes here
-  })
-
-})
+    // build component
+    const Constructor = Vue.extend(List);
+    const ListComponent = new Constructor().$mount();
+    expect(ListComponent.$el.textcontent).to.contain('play games');
+  });
+});
