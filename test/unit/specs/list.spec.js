d370bad352c6d8bce85d6ecd36e21a1d80e05b48
Harish Ramachandran <harishsr@icloud.com>
2017-07-23 10:24:50 -0400

    Test adding new items to the page & list

--- test/unit/specs/list.spec.js
+++ test/unit/specs/list.spec.js
@@ -9,4 +9,25 @@ describe('List.vue', () => {
 
     expect(ListComponent.$el.textContent).to.contain('play games');
   });
+
+  it('adds a new item to the list on click', () => {
+    // build the component
+    const Constructor = Vue.extend(List);
+    const ListComponent = new Constructor().$mount();
+
+    // add a new item
+    ListComponent.newItem = 'moss & unicorns';
+
+    // find the button to submit and create a clickEvent
+    const button = ListComponent.$el.querySelector('button');
+    const clickEvent = new window.Event('click');
+
+    // Click it & reload the page
+    button.dispatchEvent(clickEvent);
+    ListComponent._watcher.run();
+
+    // test it!
+    expect(ListComponent.$el.textContent).to.contain('moss & unicorns');
+    expect(ListComponent.listItems).to.contain('moss & unicorns');
+  });
 });
